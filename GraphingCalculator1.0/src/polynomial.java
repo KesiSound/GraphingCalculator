6532c66e976237c9044b6984c5223ffb67af86a2
KesiSound <kswkks2719@gmail.com>
2017-01-17 22:59:34 -0800

    Added polynomial class

---
+++ GraphingCalculator1.0/src/polynomial.java
@@ -0,0 +1,10 @@
+
+public class polynomial {
+	private String polynomial;
+	//Create arraylist of the polynomial
+	//Use polynomial objects to generate points
+	
+	public polynomial(String polynomial) {
+		this.polynomial = polynomial;
+	}
+}
